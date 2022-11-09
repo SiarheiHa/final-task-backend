d69025252894ed7e5fbbed1a2a971ffcd5591f1f
SiarheiHa <belposchta@gmail.com>
2022-11-09 20:43:14 +0300

    feat: add mongosettings

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,10 +3,15 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = 'PMAuser';
+const Password = 'LkTpL1LKxyoTckZ3';
+const clasterInfo = 'cluster0.jjvndnd';
+const projectName = 'PMA';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    // await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/${projectName}`);
+    await mongoose.connect('mongodb+srv://PMAuser:LkTpL1LKxyoTckZ3@cluster0.jjvndnd.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
